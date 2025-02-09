de68e2f696938a4acbf8786d1008b3ee9771527c
Pavel Covali <covali.pavel.developer@gmail.com>
2025-02-09 17:51:46 +0200

    Use currency enum.

---
+++ Covali.Enums/Currency.cs
@@ -0,0 +1,223 @@
+﻿namespace Covali.Enums;
+
+/// <summary>
+/// Defines a set of predefined currency codes
+/// identified by their respective numeric <c>ISO 4217</c> codes.
+/// </summary>
+public enum Currency : ushort
+{
+    /// <summary>
+    /// None.
+    /// </summary>
+    NON = 0,
+
+    /// <summary>
+    /// Albanian lek.
+    /// </summary>
+    ALL = 008,
+
+    /// <summary>
+    /// Australian Dollar.
+    /// </summary>
+    AUD = 036,
+
+    /// <summary>
+    /// Armenian Dram.
+    /// </summary>
+    AMD = 051,
+
+    /// <summary>
+    /// Canadian Dollar.
+    /// </summary>
+    CAD = 124,
+
+    /// <summary>
+    /// Chinese yuan renminbi.
+    /// </summary>
+    CNY = 156,
+
+    /// <summary>
+    /// Czech Koruna.
+    /// </summary>
+    CZK = 203,
+
+    /// <summary>
+    /// Danish Krone.
+    /// </summary>
+    DKK = 208,
+
+    /// <summary>
+    /// Hong Kong dollar.
+    /// </summary>
+    HKD = 344,
+
+    /// <summary>
+    /// Hungarian Forint.
+    /// </summary>
+    HUF = 348,
+
+    /// <summary>
+    /// Iceland Krona.
+    /// </summary>
+    ISK = 352,
+
+    /// <summary>
+    /// Indian rupee.
+    /// </summary>
+    INR = 356,
+
+    /// <summary>
+    /// Shekel Israelit.
+    /// </summary>
+    ILS = 376,
+
+    /// <summary>
+    /// Japanese Yen.
+    /// </summary>
+    JPY = 392,
+
+    /// <summary>
+    /// Kazakhstan Tenge.
+    /// </summary>
+    KZT = 398,
+
+    /// <summary>
+    /// South Korean won.
+    /// </summary>
+    KRW = 410,
+
+    /// <summary>
+    /// Kuwaiti Dinar.
+    /// </summary>
+    KWD = 414,
+
+    /// <summary>
+    /// Kyrgyzstan Som.
+    /// </summary>
+    KGS = 417,
+
+    /// <summary>
+    /// Malaysian Ringgit.
+    /// </summary>
+    MYR = 458,
+
+    /// <summary>
+    /// Moldovan leu.
+    /// </summary>
+    MDL = 498,
+
+    /// <summary>
+    /// New Zealand Dollar.
+    /// </summary>
+    NZD = 554,
+
+    /// <summary>
+    /// Norwegian Krone.
+    /// </summary>
+    NOK = 578,
+
+    /// <summary>
+    /// Russian Ruble.
+    /// </summary>
+    RUB = 643,
+
+    /// <summary>
+    /// Swedish Króna.
+    /// </summary>
+    SEK = 752,
+
+    /// <summary>
+    /// Swiss Franc.
+    /// </summary>
+    CHF = 756,
+
+    /// <summary>
+    /// U.A.E. Dirham.
+    /// </summary>
+    AED = 784,
+
+    /// <summary>
+    /// Macedonian denar.
+    /// </summary>
+    MKD = 807,
+
+    /// <summary>
+    /// Pound Sterling.
+    /// </summary>
+    GBP = 826,
+
+    /// <summary>
+    /// US Dollar.
+    /// </summary>
+    USD = 840,
+
+    /// <summary>
+    /// Uzbekistan Sum.
+    /// </summary>
+    UZS = 860,
+
+    /// <summary>
+    /// Belarusian Ruble.
+    /// </summary>
+    BYN = 933,
+
+    /// <summary>
+    /// Turkmenistan Manat.
+    /// </summary>
+    TMT = 934,
+
+    /// <summary>
+    /// Serbian Dinar.
+    /// </summary>
+    RSD = 941,
+
+    /// <summary>
+    /// Azerbaijani Manat.
+    /// </summary>
+    AZN = 944,
+
+    /// <summary>
+    /// Romanian Leu.
+    /// </summary>
+    RON = 946,
+
+    /// <summary>
+    /// Turkish Lira.
+    /// </summary>
+    TRY = 949,
+
+    /// <summary>
+    /// Special Drawing Rights.
+    /// </summary>
+    XDR = 960,
+
+    /// <summary>
+    /// Tajikistan Somoni.
+    /// </summary>
+    TJS = 972,
+
+    /// <summary>
+    /// Bulgarian Lev.
+    /// </summary>
+    BGN = 975,
+
+    /// <summary>
+    /// Euro.
+    /// </summary>
+    EUR = 978,
+
+    /// <summary>
+    /// Ukraine Hryvnia.
+    /// </summary>
+    UAH = 980,
+
+    /// <summary>
+    /// Georgian Lar.
+    /// </summary>
+    GEL = 981,
+
+    /// <summary>
+    /// Polish Zloty.
+    /// </summary>
+    PLN = 985
+}
